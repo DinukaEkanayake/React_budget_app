78505298d968cda70ad29cb250879cbc2b3fbe69
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-06-28 14:22:56 +0530

    make budget editable-set upped limit to 200000

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, setBudget } = useContext(AppContext);
+    const [inputValue, setInputValue] = useState(budget);
+  
+    const handleInputChange = (event) => {
+      const value = event.target.value;
+      setInputValue(value);
+    };
+  
+    const handleInputBlur = () => {
+        const newBudget = parseInt(inputValue);
+        if (!isNaN(newBudget)) {
+          setBudget(newBudget);
+        } else {
+          setInputValue(budget);
+        }
+      };
+    
+  
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
+      <div className='alert alert-secondary'>
+        <span>Budget: £</span>
+        <input
+          type='number'
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+        />
+      </div>
     );
-};
+  };
+  
 //Here, you are using the Bootstrap Alert classes to give a nice gray background 
 //by adding some text and hard coding a value.
 export default Budget;
